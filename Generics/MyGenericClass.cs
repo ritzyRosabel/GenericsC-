416b2aaa2a3f9d7ff2d2631108dc7c7b393e219d
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-17 21:34:35 +0100

    Generic method declaration

--- Generics/MyGenericClass.cs
+++ Generics/MyGenericClass.cs
@@ -8,11 +8,18 @@ namespace Generics
 {
     public class MyGenericClass<T>//the place holder ca be of any value
     {
-        private T genericField;
+        private readonly T genericField;
 
         public MyGenericClass(T values)
         {
             genericField = values;
+        }
+
+        public T GenericMethod(T genericParameter)
+        {
+            Console.WriteLine("Parameter type :{0}, value:{1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Field type: {0}, Value{1}", typeof(T), genericField);
+            return genericField;
         }
     }
 }
